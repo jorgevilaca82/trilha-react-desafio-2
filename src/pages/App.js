616a32bb2c4bc70dc4b3b8be1b08efe28baf295d
Jorge Vilaça <jorge.vilaca@gmail.com>
2026-05-12 11:59:04 -0300

    update deps and remove repo

--- src/pages/App.js
+++ src/pages/App.js
@@ -36,7 +36,7 @@ function App() {
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
 
-    // utilizar filter.
+    setRepos(prev => prev.filter(repo => repo.id !== id))
   }
 
 
